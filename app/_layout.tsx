b7e123edf4f476e6a0eeec1a50bb4608defe74a6
Olaleye O. Oladipo <olaleye@thebluematrix.com>
2025-09-10 12:00:26 +0100

    done with routing navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,21 @@ import { Stack } from "expo-router";
 import './global.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen
+      name="(tabs)"
+      options={{
+        headerShown: false,
+        title: "Home"
+      }}
+    />
+    
+    <Stack.Screen
+      name="movies/[id]"
+      options={{
+        headerShown: false,
+        title: "Movie Details"
+      }}
+    />
+  </Stack>;
 }
