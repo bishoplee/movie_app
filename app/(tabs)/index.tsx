b1fbe60a6618cb8a7657cd8bf60b347183d48b0b
Olaleye O. Oladipo <olaleye@thebluematrix.com>
2025-09-11 23:03:42 +0100

    sdk54 upgrade

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
+import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { Link, useRouter } from "expo-router";
-import { Image, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
 
   const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError
+  } = useFetch(() => fetchMovies({
+    query: ''
+  }));
 
   return (
     <View
@@ -30,12 +41,47 @@ export default function Index() {
           className="w-12 h-10 justify-center mt-20 mb-5 mx-auto"
         />
 
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => router.push('/search')}
-            placeholder="Search movies, TV shows..."
+        {moviesLoading ? (
+          // <Text className="text-white text-lg text-center">Loading...</Text>
+          <ActivityIndicator
+            size="large"
+            color="#e5e5e7ff"
+            className="mt-16 self-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text
+            className="text-red-500 text-lg text-center"
+          >Error: {moviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push('/search')}
+              placeholder="Search movies, TV shows..."
+            />
+                
+            <>
+              <Text className="text-white text-2xl font-bold mb-8 mt-16">Trending Movies</Text>
+            </>
+            
+            <FlatList
+              data={movies?.results}
+              renderItem={({ item }) => (
+                <MovieCard
+                  { ...item } />
+              )}
+              keyExtractor={(item) => item.id.toString()}
+              numColumns={3}
+              columnWrapperStyle={{
+                justifyContent: 'flex-start',
+                gap: 4,
+                // paddingRight: 4,
+                marginBottom: 16
+              }}
+              className="mt-4 pb-32"
+              scrollEnabled={false}
+            />
+          </View>
+        )}
         
       </ScrollView>
     </View>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,27 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import './global.css';
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen
-      name="(tabs)"
-      options={{
-        headerShown: false,
-        title: "Home"
-      }}
-    />
-    
-    <Stack.Screen
-      name="movies/[id]"
-      options={{
-        headerShown: false,
-        title: "Movie Details"
-      }}
-    />
-  </Stack>;
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+            title: "Home"
+          }}
+        />
+        
+        <Stack.Screen
+          name="movies/[id]"
+          options={{
+            headerShown: false,
+            title: "Movie Details"
+          }}
+        />
+      </Stack>
+    </SafeAreaProvider>
+  );
 }
